d343cad43221baab611cf8970500e34f4a192e77
victorsilva212 <victorcntres@gmail.com>
2025-07-29 21:47:39 -0300

    Aula02Eclipse

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao02/aula02/Text01.java
@@ -0,0 +1,14 @@
+package br.com.senaisp.bauru.secao02.aula02;
+
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
