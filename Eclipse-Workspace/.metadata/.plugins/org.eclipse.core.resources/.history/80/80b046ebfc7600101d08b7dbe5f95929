09c6344a65ffae59344b48d2cb634fd34b3e9b8a
victorsilva212 <victorcntres@gmail.com>
2025-08-11 21:42:55 -0300

    Aula10

---
+++ Eclipse-Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/80/80b046ebfc7600101d08b7dbe5f95929
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.secao04.aula08;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class NameMaker {
+
+	public static void main(String[] args) {
+		//Solicitar o nome, nome do meio e sobrenome separadamente
+		//Mostrar o nome completo ao término 
+		//pode usar o Scanner ou JoptionPane para solicitar ao usuário
+	String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+	}
+
+}
--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao04/aula08/NameMaker.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao04/aula08/NameMaker.java
@@ -11,6 +11,10 @@ public class NameMaker {
 		//Mostrar o nome completo ao término 
 		//pode usar o Scanner ou JoptionPane para solicitar ao usuário
 	String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+	String nomeMeio = JOptionPane.showInputDialog("Agora nome do meio: ");
+	String sobrenome = JOptionPane.showInputDialog("Agora sobrenome: ");
+	String nomeCompleto = nome + " " + nomeMeio + " " + sobrenome;
+	JOptionPane.showMessageDialog(null, "Seu nome completo é " + nomeCompleto);
 	}
 
 }
