f017f8f5120a6f3e29760fcca535178a09558ccb
victorsilva212 <victorcntres@gmail.com>
2025-09-01 21:47:48 -0300

    Aula21

--- Eclipse-Workspace/projeto_banco_de_dados/src/br/com/senaisp/bauru/victor/classes/Produto.java
+++ Eclipse-Workspace/projeto_banco_de_dados/src/br/com/senaisp/bauru/victor/classes/Produto.java
@@ -25,6 +25,9 @@ public class Produto {
 		this.preco = preco;
 		conn = ConectorBancoDados.getInstancia();
 	}
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
 	public String getDescricao() {
 		return descricao;
 	}
